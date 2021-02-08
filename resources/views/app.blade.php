e691d759e51eaffd6ba59a84ff8f72c6daab5b60
fender1990 <fabio.mancin90@gmail.com>
2021-02-08 11:41:07 +0100

    Favicon and bootstrap via node module

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,18 +3,15 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <!--<meta name="csrf-token" content="{{ csrf_token() }}">-->
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Recycle</title>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+      <link rel="icon" href="{{ URL::asset('images/favicon.svg') }}" type="image/x-icon"/>
       <link href="/css/app.css" rel="stylesheet">
    </head>
    <body>
       <div id="root">
          @yield('content')
       </div>
-
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
       <script src="/js/app.js"></script>
    </body>
 </html>
